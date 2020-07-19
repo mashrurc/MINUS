b37ab62f7fb3c7f6588c2924821259634d058075
miphc42 <4hmin2@gmail.com>
2020-07-19 09:59:15 -0400

    p

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:adobe_xd/page_link.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'circle.dart';
 
 void main() {
   runApp(SignUpPage());
