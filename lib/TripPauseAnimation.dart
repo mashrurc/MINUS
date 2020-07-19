2de2980065eb7946e44c7b65ab567ef459e6ad98
miphc42 <4hmin2@gmail.com>
2020-07-18 21:10:24 -0400

    pause

--- lib/TripPauseAnimation.dart
+++ lib/TripPauseAnimation.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:adobe_xd/page_link.dart';
 // import './EndofTrip.dart';
-
+//trippause
 class TripPauseAnimation extends StatelessWidget {
   TripPauseAnimation({
     Key key,
