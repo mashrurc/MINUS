ca687ed41ad2c755bd0b0539044b4f6e063bef54
miphc42 <4hmin2@gmail.com>
2020-07-18 21:31:41 -0400

    component

---
+++ lib/Component74.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Component74 extends StatelessWidget {
+  Component74({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        SvgPicture.string(
+          _svg_hjfzdk,
+          allowDrawingOutsideViewBox: true,
+        ),
+        Transform.translate(
+          offset: Offset(307.0, 194.88),
+          child: Text(
+            'Sun',
+            style: TextStyle(
+              fontFamily: 'AvertaStd ☞',
+              fontSize: 10,
+              color: const Color(0xffffffff),
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Transform.translate(
+          offset: Offset(155.0, 194.88),
+          child: Text(
+            'Thu',
+            style: TextStyle(
+              fontFamily: 'AvertaStd ☞',
+              fontSize: 10,
+              color: const Color(0xffffffff),
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Transform.translate(
+          offset: Offset(105.0, 194.88),
+          child: Text(
+            'Wed',
+            style: TextStyle(
+              fontFamily: 'AvertaStd ☞',
+              fontSize: 10,
+              color: const Color(0xffffffff),
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Transform.translate(
+          offset: Offset(58.0, 194.88),
+          child: Text(
+            'Tue',
+            style: TextStyle(
+              fontFamily: 'AvertaStd ☞',
+              fontSize: 10,
+              color: const Color(0xffffffff),
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Transform.translate(
+          offset: Offset(257.0, 194.88),
+          child: Text(
+            'Sat',
+            style: TextStyle(
+              fontFamily: 'AvertaStd ☞',
+              fontSize: 10,
+              color: const Color(0xffffffff),
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Transform.translate(
+          offset: Offset(5.0, 194.88),
+          child: Text(
+            'Mon',
+            style: TextStyle(
+              fontFamily: 'AvertaStd ☞',
+              fontSize: 10,
+              color: const Color(0xffffffff),
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Transform.translate(
+          offset: Offset(209.0, 194.88),
+          child: Text(
+            'Fri',
+            style: TextStyle(
+              fontFamily: 'AvertaStd ☞',
+              fontSize: 10,
+              color: const Color(0xffffffff),
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+const String _svg_hjfzdk =
+    '<svg viewBox="0.0 0.0 333.0 188.9" ><defs><filter id="shadow"><feDropShadow dx="0" dy="2" stdDeviation="6"/></filter></defs><path transform="translate(300.0, 125.16)" d="M 0 0 L 33 0 L 33 63.71525573730469 L 0 63.71525573730469 L 0 0 Z" fill="#83f7a6" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/><defs><filter id="shadow"><feDropShadow dx="0" dy="2" stdDeviation="6"/></filter></defs><path transform="translate(250.0, 0.0)" d="M 0 0 L 33 0 L 33 187.8797912597656 L 0 187.8797912597656 L 0 0 Z" fill="#f9686e" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/><defs><filter id="shadow"><feDropShadow dx="0" dy="2" stdDeviation="6"/></filter></defs><path transform="translate(200.0, 73.88)" d="M 0 0 L 33 0 L 33 114 L 0 114 L 0 0 Z" fill="#83f7a6" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/><defs><filter id="shadow"><feDropShadow dx="0" dy="2" stdDeviation="6"/></filter></defs><path transform="translate(150.0, 33.89)" d="M 0 0 L 33 0 L 33 152.9855651855469 L 0 152.9855651855469 L 0 0 Z" fill="#f9686e" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/><defs><filter id="shadow"><feDropShadow dx="0" dy="2" stdDeviation="6"/></filter></defs><path transform="translate(100.0, 97.55)" d="M 0 0 L 33 0 L 33 90.32666015625 L 0 90.32666015625 L 0 0 Z" fill="#83f7a6" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/><defs><filter id="shadow"><feDropShadow dx="0" dy="2" stdDeviation="6"/></filter></defs><path transform="translate(50.0, 72.88)" d="M 0 0 L 33 0 L 33 115 L 0 115 L 0 0 Z" fill="#f9686e" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/><defs><filter id="shadow"><feDropShadow dx="0" dy="2" stdDeviation="6"/></filter></defs><path transform="translate(0.0, 119.26)" d="M 0 0 L 33 0 L 33 68.62408447265625 L 0 68.62408447265625 L 0 0 Z" fill="#83f7a6" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/></svg>';
