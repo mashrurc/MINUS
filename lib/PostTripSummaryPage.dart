13921a20cde7975d1caeed15c5d4ceae0d43f24e
miphc42 <4hmin2@gmail.com>
2020-07-19 00:57:37 -0400

    trip summary

--- lib/PostTripSummaryPage.dart
+++ lib/PostTripSummaryPage.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:adobe_xd/page_link.dart';
 import './ProfilePage.dart';
